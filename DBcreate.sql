5f645f550f891b21e439698b84dcfebe7ef3008f
Vladislav Kopylash <vladik.kopilash@gmail.com>
2015-09-08 18:32:59 +0300

    basic config

---
+++ DBcreate.sql
@@ -0,0 +1,26 @@
+﻿create table addresses (
+id serial primary key not null,
+city varchar(50) not null,
+street varchar(50) not null);
+
+
+
+
+create table employers (
+id serial primary key not null,
+company varchar(50) not null,
+activity_field varchar(50) not null,
+address_id int references addresses(id));
+
+create table users (
+id serial primary key not null,
+name varchar(50) not null,
+surname varchar(50) not null,
+address_id int references addresses(id),
+employers_id int references employers(id));
+
+create table deposits (
+id serial primary key not null,
+amount money not null,
+currency varchar(3) not null,
+user_id int references users(id));
---
+++ DBfill.sql
@@ -0,0 +1,22 @@
+﻿-- Database: softengi
+
+
+
+insert into addresses(city,street) values ('Kiev','Peremohy avenue');
+insert into addresses(city,street) values ('London','Picadilly Circus');
+
+insert into employers(company,activity_field,address_id) values ('Ciklum','IT',2);
+insert into employers(company,activity_field,address_id) values ('Softengi','IT',1);
+
+insert into users(name,surname,employers_id,address_id) values ('Vladislav','Kopylash',1,2);
+insert into users(name,surname,employers_id,address_id) values ('John','Doe',2,1);
+insert into users(name,surname,employers_id,address_id) values ('Svitlana','Shepeta',1,2);
+
+insert into deposits(amount,currency,user_id) values (5,'UAH',1);
+insert into deposits(amount,currency,user_id) values (3000.53,'USD',2);
+insert into deposits(amount,currency,user_id) values (1496.11,'USD',3);
+
+
+
+
+
